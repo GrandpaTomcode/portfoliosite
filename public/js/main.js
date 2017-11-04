89150b32ccb5447de2519b89f3bcee250a5ba1be
Tom Hornbuckle <tomhornbuckle1@gmail.com>
2017-11-04 20:08:02 +0000

    added node js server

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// mobile nav toggle
+
+  $( ".mobNavBtn" ).click(function() {
+    $( ".mobNav" ).slideToggle( "fast");
+  });
+
+  // mobile Nav close
+
+  $(".mobNav").click(function () {
+    $('.mobNav').slideUp('fast');
+  });

8de8f1e6d87ea55906d8a3a97373ad10a07c39d1
Tom Hornbuckle <tomhornbuckle1@gmail.com>
2017-11-07 14:39:45 +0000

    Desktop and Mob navigation buttons now work.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,36 @@
 // mobile nav toggle
 
-  $( ".mobNavBtn" ).click(function() {
-    $( ".mobNav" ).slideToggle( "fast");
-  });
+$('.mobNavBtn').click(function() {
+    $('.mobNav').slideToggle('fast');
+});
 
-  // mobile Nav close
+// mobile Nav close
 
-  $(".mobNav").click(function () {
+$('.mobNav').click(function() {
     $('.mobNav').slideUp('fast');
-  });
+});
+
+$('.aboutBtn').click(function() {
+    $('html, body').animate(
+        {
+            scrollTop: $('.aboutContainer').offset().top
+        },
+        500
+    );
+});
+$('.myWorkBtn').click(function() {
+    $('html, body').animate(
+        {
+            scrollTop: $('.previousWorkContainer').offset().top
+        },
+        500
+    );
+});
+$('.letsWorkBtn').click(function() {
+    $('html, body').animate(
+        {
+            scrollTop: $('.contactFormContainer').offset().top
+        },
+        500
+    );
+});

02e5285c5d4444954c0a47905f321028c4a1c768
Tom Hornbuckle <tomhornbuckle1@gmail.com>
2017-11-09 00:56:00 +0000

    Changes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+
 app.use(express.static(path.join(__dirname, '../public')));
 
+
+
 app.listen(9000, () => {
-    console.log('listening to port: 9000');
+    console.log('[ya boi is listening on port 9000]');
 });

8706131928363baac7e28e2ac0adf8ac060c8cba
Tom Hornbuckle <tomhornbuckle1@gmail.com>
2017-12-13 13:33:03 +0000

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+// const nodemailer = require('nodemailer');
+// const bodyParser = require('body-parser');
+// const jsonParserFunc = require('./boot/setupJsonParser').setupJsonParser;
 const path = require('path');
 const app = express();
-
-
-
+// const jsonParser = bodyParser.json({
+//         limit: '10mb',
+//         type: 'application/json',
+//     });
+//
+//
+// app.use(jsonParser);
 app.use(express.static(path.join(__dirname, '../public')));
-
-
 
 app.listen(9000, () => {
     console.log('[ya boi is listening on port 9000]');

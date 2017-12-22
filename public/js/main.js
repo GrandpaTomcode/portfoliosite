68b898b19dcc6864074275be7bf57eae6625d08b
Tom Hornbuckle <tomhornbuckle1@gmail.com>
2017-12-22 13:54:41 +0000

    blah blah blah

---
+++ public/js/main.js
@@ -0,0 +1,116 @@
+$(document).ready(function() {
+  // mobile nav toggle
+  $(".mobNavBtn").click(function() {
+    $(".mobNav").slideToggle("fast");
+  });
+
+  // mobile Nav close
+
+  $(".mobNav").click(function() {
+    $(".mobNav").slideUp("fast");
+  });
+
+  $(".aboutBtn").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $(".aboutContainer").offset().top
+      },
+      500
+    );
+  });
+  $(".myWorkBtn").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $(".previousWorkContainer").offset().top
+      },
+      500
+    );
+  });
+  $(".letsWorkBtn").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $(".contactFormContainer").offset().top
+      },
+      500
+    );
+  });
+});
+
+const submitBtn = document.getElementById("submitButton");
+
+submitBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  const emailValidation = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const inputValidation = input => {
+    return input.length > 0;
+  };
+
+  const name = document.getElementById("name").value;
+  const subject = document.getElementById("subject").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  // Validation
+  const emailValid = emailValidation.test(email);
+  const nameValid = inputValidation(name);
+  const subjectValid = inputValidation(subject);
+  const messageValid = inputValidation(message);
+
+  if (emailValid !== true) {
+    const invalidEmail = document.getElementById("invalidEmail");
+    $(".invalidEmail").addClass("showInvalid");
+  }
+
+  const inputValid = [];
+  if (nameValid) {
+    inputValid.push(nameValid);
+  }
+  if (emailValid) {
+    inputValid.push(emailValid);
+  }
+  if (subjectValid) {
+    inputValid.push(subjectValid);
+  }
+  if (messageValid) {
+    inputValid.push(messageValid);
+  }
+
+  console.log(name, subject, email, message);
+  if (inputValid.length >= 3) {
+    // let data = { name, subject, email, message };
+    // try {
+    //   data = JSON.stringify(data);
+    // } catch (err) {
+    //   console.log(err);
+    // }
+    console.log({ name, subject, email, message });
+    event.preventDefault();
+    axios
+      .get("/submit", {
+        params: {
+          Name: name,
+          Subject: subject,
+          Email: email,
+          Message: message
+        }
+        event.preventDefault()
+        axios.get('/submit', {
+                params: {
+                    data
+                }
+            })
+            .then(function (response) {
+                console.log(response);
+                if (response) {
+                    console.log(response);
+                }
+            })
+            .catch(function (error) {
+                console.log(
+                    'An error occured while attempting to send the contact information to the server: ' +
+                    error
+                );
+            })
+    } else{
+        console.log(inputValid);
+        console.log('one or more parameters in the form are invalid');
+    }

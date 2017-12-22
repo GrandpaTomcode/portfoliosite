14ff3fa404927222c71a908a075144375300aebd
ozzie1998 <ozziepeck11@gmail.com>
2017-12-22 13:33:16 +0000

    using get instead of post - works now

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const nodemailer = require('nodemailer')
-const bodyParser = require('body-parser');
-const fs = require('fs')
-const path = require('path');
-const submit = require('./sendEmail.js').submit;
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+const submit = require("./sendEmail.js").submit;
 const app = express();
-let deps = {}
-let config
+let deps = {};
+let config;
 
 try {
-    config = JSON.parse(fs.readFileSync('config.json', 'utf-8'))
+  config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
 } catch (e) {
-    console.log(e)
+  console.log(e);
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.post('/submit', (req, res) => {
-    deps.config = config
-    submit(req, res, deps)
-})
+app.get("/submit", (req, res) => {
+  deps.config = config;
+  submit(req, res, deps);
+});
 
 app.listen(9000, () => {
-    console.log('[ya boi is listening on port 9000]');
+  console.log("[ya boi is listening on port 9000]");
 });

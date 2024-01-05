2edc3407edab7a56563deb2a642f0c5c29bb1dfa
tom <tomhornbuckle1@gmail.com>
2024-01-05 12:41:24 +0000

    Changes

--- server/server.js
+++ server/server.js
@@ -1,51 +1,44 @@
-const express = require("express")
-const nodemailer = require("nodemailer")
-const bodyParser = require("body-parser")
-const https = require('https')
-const fs = require("fs")
-const helmet = require("helmet")
-const path = require("path")
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const https = require("https");
+const fs = require("fs");
+const helmet = require("helmet");
+const path = require("path");
 const submit = require("./sendEmail.js").submit;
-const config = require("./config.json")
-const app = express()
 
-let deps = {}
+const app = express();
+
+let deps = {};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
-
-
 app.get("/submit", (req, res) => {
   deps.config = config;
-  console.log(req.query.data)
-  submit(req, res, deps).then(result => {
-    res.send("Sent and Recieved")
-  }).catch(e => { console.log(e) })
+  console.log(req.query.data);
+  submit(req, res, deps)
+    .then(result => {
+      res.send("Sent and Recieved");
+    })
+    .catch(e => {
+      console.log(e);
+    });
 });
 
 app.get("/blog", (req, res) => {
-  app.use(express.static(path.join(__dirname, "../blog")))
-  res.sendFile(path.join(__dirname, '../blog/blog.html'))
-
-})
+  app.use(express.static(path.join(__dirname, "../blog")));
+  res.sendFile(path.join(__dirname, "../blog/blog.html"));
+});
 app.get("/sitemap", (req, res) => {
-console.log(__dirname)
-  res.sendFile(path.join(__dirname, '/sitemap.html'))
-})
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, "/sitemap.html"));
+});
 
 app.get("/personalHomepage", (req, res) => {
-  app.use(express.static(path.join(__dirname, "../personalHomepage")))
-  res.sendFile(path.join(__dirname, '../personalHomepage/index.html'))
-
-})
+  app.use(express.static(path.join(__dirname, "../personalHomepage")));
+  res.sendFile(path.join(__dirname, "../personalHomepage/index.html"));
+});
 // Server listening
-let options;
-if (!config.production){
-   options = {
-    cert: fs.readFileSync('/etc/letsencrypt/live/papasodiepop.me/fullchain.pem'),
-    key: fs.readFileSync('/etc/letsencrypt/live/papasodiepop.me/privkey.pem')
-}}
-app.use(require('helmet')());
-app.listen(8080, () => console.log("started the server on port 8080"))
-if(!config.production) https.createServer(options, app).listen(443)
+
+app.listen(8080, () => console.log("started the server on port 8080"));
